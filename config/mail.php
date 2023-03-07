b4fed8b13070576ab57c31d378383f1d379a1d6f
Alistair <alistairgrant95@gmail.com>
2023-03-07 11:41:21 +0000

    some changes to config

--- config/mail.php
+++ config/mail.php
@@ -39,8 +39,8 @@ return [
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MY_EMAIL'),
+            'password' => env('MY_EMAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
@@ -98,8 +98,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MY_EMAIL'),
+        'name' => env('MAIL_FROM_NAME', 'Me'),
     ],
 
     /*
