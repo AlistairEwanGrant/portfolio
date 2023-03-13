23240455dba4e243418027fe9b178f6d955e0fe1
Alistair <alistairgrant95@gmail.com>
2023-03-13 14:21:32 +0000

    try apii

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,8 @@ Route::get('/contact', function () {
 Route::get('/curriculum-vitae/download', [CurriculumVitaeController::class, 'index'])->name('curriculumVitae.index');
 Route::put('/mail', [MailController::class, 'store'])->name('mail');
 
-
+Route::get('/company-directory', function () {
+    return view('company/company');
+})->name('company');
 
 require __DIR__ . '/auth.php';
